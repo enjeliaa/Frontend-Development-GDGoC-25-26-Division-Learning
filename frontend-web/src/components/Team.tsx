567181500907f8cc537c36d96cf79ed082f07e27
ElloRabyndra <Ellorabyndra123@gmail.com>
2026-01-02 23:35:23 +0700

    ui: change member list grid column

--- frontend-web/src/components/Team.tsx
+++ frontend-web/src/components/Team.tsx
@@ -44,7 +44,7 @@ const Team = () => {
         </motion.div>
 
         {/* Members Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {members.map((member, index) => (
             <TeamCard
               key={member.name}
